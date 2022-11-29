7b12102a4ad23f89bb8108e639c2c1d6162f5942
Mathias Scherer <scherer.mat@gmail.com>
2022-11-29 13:21:28 +0100

    fix: starter.sh

--- starter.sh
+++ starter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "postgres" > /tmp/password
-initdb -U postgres -D /tmp/data -A password --pwfile /tmp/password 
+echo "password" > /tmp/password
+initdb -U noty -D /tmp/data -A password --pwfile /tmp/password 
 pg_ctl -D /tmp/data -l logfile start
 
-export PGPASSWORD=postgres
-psql -U postgres postgres -c "create database noty;"
+export PGPASSWORD=password
+psql -U noty noty -c "create database noty;"
 
 node dist/index.js
